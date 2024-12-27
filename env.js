b7ae5a167e2a4d31ed852e4c3d588cd8541dd28e
Sarb <web2it@hotmail.co.uk>
2024-12-27 10:43:17 +0530

    apk

--- env.js
+++ env.js
@@ -1,3 +1,12 @@
 export const API_URL = "https://mart-api-v2.vercel.app/api/v1/"
 
 // # ngrok working command :  ngrok http https://localhost:4200
+
+// follow these step to make build in react native 
+
+// step 1 - npm  instal -g eas-cli
+
+// step 2 - eas build -p android
+ 
+// eas build -p android --profile preview
+ 
